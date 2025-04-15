6670d4d990e0cb487b54b87dbd98a84922920309
Petar <taneski001@gmail.com>
2025-04-15 17:11:37 +0200

    no message

--- src/components/Home/HeroSection/Hero.tsx
+++ src/components/Home/HeroSection/Hero.tsx
@@ -12,13 +12,13 @@ const Hero = () => {
           style={{ backgroundImage: "url('/images/header.png')" }}
         >
           {/* Content overlay */}
-          <div className="h-full min-h-screen flex items-center justify-center text-white">
-            <div className="container mx-auto px-4">
-              <div className="flex flex-col pr-10 gap-2 pb-30 w-full text-center">
+          <div className="flex items-center justify-center h-full min-h-screen text-white">
+            <div className="container px-4 mx-auto">
+              <div className="flex flex-col w-full gap-2 text-center md:pr-10 pb-30">
                 <h1 className="text-6xl font-extrabold text-white/98">
                   {t("about.shortName")}
                 </h1>
-                <span className="text-gray-100/80 text-lg break-words ">
+                <span className="text-lg break-words text-gray-100/80 ">
                   {t("about.name")}
                 </span>
               </div>
