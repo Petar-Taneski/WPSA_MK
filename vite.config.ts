1614bde4108b2dbff347c2a8e4e00d666560bbf0
Petar <taneski001@gmail.com>
2025-04-15 20:11:34 +0200

    Revert GitHub Pages deployment changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/WPSA_MK/",
   plugins: [react(), tailwindcss()],
   publicDir: "public",
   server: {
